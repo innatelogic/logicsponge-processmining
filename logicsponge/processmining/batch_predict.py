1630ef991523299c950a41cc9b48b6aea6094b89
bollig <bollig@lmf.cnrs.fr>
2024-12-01 10:31:37 +0100

    ...

--- logicsponge/processmining/batch_predict.py
+++ logicsponge/processmining/batch_predict.py
@@ -135,7 +135,7 @@ for iteration in range(n_iterations):
     fallback = Fallback(
         models=[
             BasicMiner(algorithm=FrequencyPrefixTree(min_total_visits=1)),
-            BasicMiner(algorithm=NGram(window_length=5)),
+            BasicMiner(algorithm=NGram(window_length=4)),
         ],
         config=config,
     )
