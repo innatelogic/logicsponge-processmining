c73a6395d1c2b732114e0e488a2cf5f918594703
bollig <bollig@lmf.cnrs.fr>
2024-12-01 09:26:03 +0100

    ...

--- logicsponge/processmining/batch_predict.py
+++ logicsponge/processmining/batch_predict.py
@@ -40,7 +40,7 @@ torch.cuda.manual_seed(123)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-NN_training = True
+NN_training = False
 
 # ============================================================
 # Data preparation
