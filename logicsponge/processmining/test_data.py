03fc147c983b29073f3e8deefc9d8901468c9767
bollig <bollig@lmf.cnrs.fr>
2024-12-01 19:14:17 +0100

    ...

--- logicsponge/processmining/test_data.py
+++ logicsponge/processmining/test_data.py
@@ -95,7 +95,7 @@ data_collection = {
 # ============================================================
 
 if DATA == "file":
-    data_name = "BPI_Challenge_2013"
+    data_name = "BPI_Challenge_2014"
     data = data_collection[data_name]
     file_path = os.path.join(FOLDERNAME, data["target_filename"])
     data["file_path"] = file_path
