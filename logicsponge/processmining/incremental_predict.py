185c122004e41cfe33373953921b62f23d7e6069
bollig <bollig@lmf.cnrs.fr>
2024-12-02 20:11:53 +0100

    ...

--- logicsponge/processmining/incremental_predict.py
+++ logicsponge/processmining/incremental_predict.py
@@ -318,19 +318,19 @@ lstm = StreamingActionPredictor(
 # Model names
 models = [
     "fpt",
-    # "bag",
-    # "ngram_1",
-    # "ngram_2",
-    # "ngram_3",
-    # "ngram_4",
-    # "ngram_5",
-    # "ngram_6",
-    # "ngram_7",
-    # "ngram_8",
-    "fallback",
-    "hard_voting",
-    "soft_voting",
-    "adaptive_voting",
+    "bag",
+    "ngram_1",
+    "ngram_2",
+    "ngram_3",
+    "ngram_4",
+    "ngram_5",
+    "ngram_6",
+    "ngram_7",
+    "ngram_8",
+    # "fallback",
+    # "hard_voting",
+    # "soft_voting",
+    # "adaptive_voting",
     # "lstm",
 ]
 
@@ -350,19 +350,19 @@ sponge = (
     * AddStartSymbol()
     * (
         (fpt * Evaluation("fpt"))
-        # | (bag * Evaluation("bag"))
-        # | (ngram_1 * Evaluation("ngram_1"))
-        # | (ngram_2 * Evaluation("ngram_2"))
-        # | (ngram_3 * Evaluation("ngram_3"))
-        # | (ngram_4 * Evaluation("ngram_4"))
-        # | (ngram_5 * Evaluation("ngram_5"))
-        # | (ngram_6 * Evaluation("ngram_6"))
-        # | (ngram_7 * Evaluation("ngram_7"))
-        # | (ngram_8 * Evaluation("ngram_8"))
-        | (fallback * Evaluation("fallback"))
-        | (hard_voting * Evaluation("hard_voting"))
-        | (soft_voting * Evaluation("soft_voting"))
-        | (adaptive_voting * Evaluation("adaptive_voting"))
+        | (bag * Evaluation("bag"))
+        | (ngram_1 * Evaluation("ngram_1"))
+        | (ngram_2 * Evaluation("ngram_2"))
+        | (ngram_3 * Evaluation("ngram_3"))
+        | (ngram_4 * Evaluation("ngram_4"))
+        | (ngram_5 * Evaluation("ngram_5"))
+        | (ngram_6 * Evaluation("ngram_6"))
+        | (ngram_7 * Evaluation("ngram_7"))
+        | (ngram_8 * Evaluation("ngram_8"))
+        # | (fallback * Evaluation("fallback"))
+        # | (hard_voting * Evaluation("hard_voting"))
+        # | (soft_voting * Evaluation("soft_voting"))
+        # | (adaptive_voting * Evaluation("adaptive_voting"))
         # | (lstm * Evaluation("lstm"))
     )
     * ls.ToSingleStream(flatten=True)
