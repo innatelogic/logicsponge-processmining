681de37f466f60228213080b80e6daa4e6d8c0b6
bollig <bollig@lmf.cnrs.fr>
2024-12-02 23:50:14 +0100

    ...

--- logicsponge/processmining/incremental_predict.py
+++ logicsponge/processmining/incremental_predict.py
@@ -325,10 +325,10 @@ models = [
     # "ngram_4",
     # "ngram_5",
     # "ngram_6",
-    "ngram_7",
-    "ngram_8",
-    # "fallback",
-    # "hard_voting",
+    # "ngram_7",
+    # "ngram_8",
+    "fallback",
+    "hard_voting",
     # "soft_voting",
     # "adaptive_voting",
     # "lstm",
@@ -357,10 +357,10 @@ sponge = (
         # | (ngram_4 * Evaluation("ngram_4"))
         # | (ngram_5 * Evaluation("ngram_5"))
         # | (ngram_6 * Evaluation("ngram_6"))
-        (ngram_7 * Evaluation("ngram_7"))
-        | (ngram_8 * Evaluation("ngram_8"))
-        # | (fallback * Evaluation("fallback"))
-        # | (hard_voting * Evaluation("hard_voting"))
+        # | (ngram_7 * Evaluation("ngram_7"))
+        # | (ngram_8 * Evaluation("ngram_8"))
+        (fallback * Evaluation("fallback"))
+        | (hard_voting * Evaluation("hard_voting"))
         # | (soft_voting * Evaluation("soft_voting"))
         # | (adaptive_voting * Evaluation("adaptive_voting"))
         # | (lstm * Evaluation("lstm"))
