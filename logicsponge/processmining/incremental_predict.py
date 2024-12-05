9f2efe8bb31756538561f9cac171117e1d5e9034
bollig <bollig@lmf.cnrs.fr>
2024-12-05 07:44:57 +0100

    ...

--- logicsponge/processmining/incremental_predict.py
+++ logicsponge/processmining/incremental_predict.py
@@ -321,16 +321,16 @@ models = [
     "bag",
     "ngram_1",
     "ngram_2",
-    "ngram_3",
-    "ngram_4",
-    "ngram_5",
-    "ngram_6",
-    "ngram_7",
-    "ngram_8",
-    "fallback",
-    "hard_voting",
-    "soft_voting",
-    "adaptive_voting",
+    # "ngram_3",
+    # "ngram_4",
+    # "ngram_5",
+    # "ngram_6",
+    # "ngram_7",
+    # "ngram_8",
+    # "fallback",
+    # "hard_voting",
+    # "soft_voting",
+    # "adaptive_voting",
     # "lstm",
 ]
 
@@ -353,16 +353,16 @@ sponge = (
         | (bag * Evaluation("bag"))
         | (ngram_1 * Evaluation("ngram_1"))
         | (ngram_2 * Evaluation("ngram_2"))
-        | (ngram_3 * Evaluation("ngram_3"))
-        | (ngram_4 * Evaluation("ngram_4"))
-        | (ngram_5 * Evaluation("ngram_5"))
-        | (ngram_6 * Evaluation("ngram_6"))
-        | (ngram_7 * Evaluation("ngram_7"))
-        | (ngram_8 * Evaluation("ngram_8"))
-        | (fallback * Evaluation("fallback"))
-        | (hard_voting * Evaluation("hard_voting"))
-        | (soft_voting * Evaluation("soft_voting"))
-        | (adaptive_voting * Evaluation("adaptive_voting"))
+        # | (ngram_3 * Evaluation("ngram_3"))
+        # | (ngram_4 * Evaluation("ngram_4"))
+        # | (ngram_5 * Evaluation("ngram_5"))
+        # | (ngram_6 * Evaluation("ngram_6"))
+        # | (ngram_7 * Evaluation("ngram_7"))
+        # | (ngram_8 * Evaluation("ngram_8"))
+        # | (fallback * Evaluation("fallback"))
+        # | (hard_voting * Evaluation("hard_voting"))
+        # | (soft_voting * Evaluation("soft_voting"))
+        # | (adaptive_voting * Evaluation("adaptive_voting"))
         # | (lstm * Evaluation("lstm"))
     )
     * ls.ToSingleStream(flatten=True)
