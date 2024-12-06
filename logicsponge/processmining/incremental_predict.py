140fd0e2b46708e80a4047ec5cf9ce9adcf7d920
bollig <bollig@lmf.cnrs.fr>
2024-12-07 00:28:08 +0100

    ...

--- logicsponge/processmining/incremental_predict.py
+++ logicsponge/processmining/incremental_predict.py
@@ -325,14 +325,14 @@ lstm = StreamingActionPredictor(
 
 # Model names
 models = [
-    "fpt",
-    "bag",
+    # "fpt",
+    # "bag",
     # "ngram_1",
     # "ngram_2",
-    # "ngram_3",
-    # "ngram_4",
-    # "ngram_5",
-    # "ngram_6",
+    "ngram_3",
+    "ngram_4",
+    "ngram_5",
+    "ngram_6",
     # "ngram_7",
     # "ngram_8",
     # "fallback",
@@ -357,14 +357,14 @@ sponge = (
     * ls.KeyFilter(keys=["case_id", "action"])
     * AddStartSymbol()
     * (
-        (fpt * Evaluation("fpt"))
-        | (bag * Evaluation("bag"))
+        # (fpt * Evaluation("fpt"))
+        # | (bag * Evaluation("bag"))
         # | (ngram_1 * Evaluation("ngram_1"))
         # | (ngram_2 * Evaluation("ngram_2"))
-        # | (ngram_3 * Evaluation("ngram_3"))
-        # | (ngram_4 * Evaluation("ngram_4"))
-        # | (ngram_5 * Evaluation("ngram_5"))
-        # | (ngram_6 * Evaluation("ngram_6"))
+        (ngram_3 * Evaluation("ngram_3"))
+        | (ngram_4 * Evaluation("ngram_4"))
+        | (ngram_5 * Evaluation("ngram_5"))
+        | (ngram_6 * Evaluation("ngram_6"))
         # | (ngram_7 * Evaluation("ngram_7"))
         # | (ngram_8 * Evaluation("ngram_8"))
         # | (fallback * Evaluation("fallback"))
