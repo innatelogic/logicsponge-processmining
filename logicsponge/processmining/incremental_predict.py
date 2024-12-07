9cae213d404edc3ab92cf1fe063dbc2ab16bd2ef
bollig <bollig@lmf.cnrs.fr>
2024-12-07 22:19:52 +0100

    ...

--- logicsponge/processmining/incremental_predict.py
+++ logicsponge/processmining/incremental_predict.py
@@ -368,7 +368,7 @@ sponge = (
     * ls.ToSingleStream(flatten=True)
     * ls.AddIndex(key="index")
     * ls.KeyFilter(keys=all_attributes)
-    * ls.DataItemFilter(data_item_filter=lambda item: item["index"] % 100 == 0 or item["index" == 1595922])
+    * ls.DataItemFilter(data_item_filter=lambda item: item["index"] % 100 == 0 or item["index"] == 1595922)
     * ls.Print()
     # * (dashboard.Plot("Accuracy (%)", x="index", y=accuracy_list))
     # * (dashboard.Plot("Latency Mean (ms)", x="index", y=latency_mean_list))
