55dba263c56d19d02e5e690afb995b3d0245a708
bollig <bollig@lmf.cnrs.fr>
2025-04-09 15:13:01 +0200

    minor

--- logicsponge/processmining/test_data.py
+++ logicsponge/processmining/test_data.py
@@ -168,9 +168,9 @@ data_collection = {
 # ============================================================
 
 if DATA == "file":
-    data_name = "Sepsis_Cases"
+    # data_name = "Sepsis_Cases"
     # data_name = "Helpdesk"
-    # data_name = "BPI_Challenge_2013"
+    data_name = "BPI_Challenge_2013"
     mydata = data_collection[data_name]  # type: ignore
     file_path = os.path.join(FOLDERNAME, mydata["target_filename"])
     mydata["file_path"] = file_path
